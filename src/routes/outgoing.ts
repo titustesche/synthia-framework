626d0cf82b30254fdd2a53144ad5c52b6a08261f
titustesche <titustesche@outlook.de>
2024-06-26 11:13:42 +0200

    Added a simple script to store the received information

--- src/routes/outgoing.ts
+++ src/routes/outgoing.ts
@@ -1,18 +1,33 @@
 import { Router } from 'express';
 import { z } from "zod";
 import {processRequestBody} from "zod-express-middleware";
+import {Memory} from "../database/entities/Memory";
 const userRoute = Router();
 const format = z.object({
     topic: z.string(),
     keywords: z.array(z.string()),
     description: z.string(),
     context: z.string(),
-    createdAt: z.date(),
-    importance: z.number(),
+    createdAt: z.string().datetime(),
+    // Workaround for floating point values that I selfishly copied from the internet
+    importance: z.number().refine( n => {
+        return n.toString().split('.')[1].length <= 2
+
+    }, {message: 'Max precision is 2 decimal places'} ),
 })
 
-userRoute.get('/', processRequestBody(format), (req, res) => {
+userRoute.post('/request', processRequestBody(format), async (req, res) => {
+    const memory = new Memory();
+    memory.topic = req.body.topic;
+    memory.keywords = JSON.stringify(req.body.keywords);
+    memory.description = req.body.description;
+    memory.context = req.body.context;
+    memory.createdAt = new Date(req.body.createdAt);
+    memory.importance = req.body.importance;
 
+    await memory.save();
+
+    res.status(200).json(memory);
 });
 
 export default userRoute;
