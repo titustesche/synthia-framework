f0fec818e8c481fe4b03f1a9bcc9a0bbb6bf7d1b
titustesche <titustesche@outlook.de>
2024-09-03 00:19:57 +0200

    Updated the Data Source with the new entities

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,5 +1,7 @@
 import {DataSource} from 'typeorm';
 import {Memory} from "./entities/Memory";
+import {Conversation} from "./entities/Conversation";
+import {Message} from "./entities/Message";
 
 export const AppDataSource = new DataSource({
     type: 'mariadb',
@@ -8,6 +10,6 @@ export const AppDataSource = new DataSource({
     username: 'ai_memory',
     password: '1234',
     database: 'ai_memory',
-    entities: [Memory],
+    entities: [Memory, Conversation, Message],
     synchronize: true
 });
