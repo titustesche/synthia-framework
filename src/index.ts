c7338da0e3707fcc9e14d2c03bdd19be07f056e2
titustesche <titustesche@outlook.de>
2024-11-25 21:29:50 +0100

    Added chatRoute to use

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import databaseRoute from "./routes/database";
 import {AppDataSource} from "./database/data-source";
 import actionRoute from "./routes/actions";
 import conversationRoute from "./routes/conversation";
+import chatRoute from "./routes/chat";
 // Keep these as require even if your IDE tells you otherwise, it won't work otherwise, dunno why
 const cors = require("cors");
 const Express = require("express");
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(databaseRoute);
 app.use(actionRoute);
 app.use(conversationRoute);
+app.use(chatRoute);
 
 const port = 3000;
 
