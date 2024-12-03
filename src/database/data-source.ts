8324e099492707cf8c053ae74842ff3f98af6a6e
titus <titustesche@outlook.de>
2024-12-03 16:44:36 +0100

    Changed Database login credentials

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -8,9 +8,9 @@ export const AppDataSource = new DataSource({
     type: 'mariadb',
     host: 'localhost',
     port: 3306,
-    username: 'ai_memory',
+    username: 'synthia_framework',
     password: '1234',
-    database: 'ai_memory',
+    database: 'synthia_framework',
     entities: [Memory, Conversation, Message, User],
     synchronize: true
 });
