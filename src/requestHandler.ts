22a01a91328177fc6873acf2a274b6722142d04d
titus <titustesche@outlook.de>
2024-12-06 00:02:42 +0100

    Removed aiRequest Method as it's no longer needed

--- src/requestHandler.ts
+++ src/requestHandler.ts
@@ -21,38 +21,3 @@ export async function getMessages(conversation: Conversation) {
 
     return cleanMessages.length > 0 ? cleanMessages : undefined;
 }
-
-export async function aiRequest(model: string, role: string, message: string, images: string[], conversation: Conversation): Promise<EventEmitter> {
-    const emitter = new EventEmitter();
-    let messages = await getMessages(conversation);
-
-    if (!messages)
-    {
-        emitter.emit("error", "Messages not found");
-    }
-
-    else {
-        let url = "http://localhost:11434/api/chat";
-        const data = {
-            "model": model,
-            "messages": messages,
-        };
-
-        try {
-            const res = await axios.post(url, data, {responseType: "stream"});
-
-            res.data.on("data", (chunk: Buffer) => {
-                // Datenverarbeitung
-                emitter.emit("data", JSON.parse(chunk.toString()).message.content);
-            });
-
-            res.data.on("end", () => {
-                emitter.emit("end");
-            });
-
-            return emitter;
-        } catch (e) {
-            emitter.emit("error", e);
-        }
-    }
-}
