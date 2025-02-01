46e78a73c665fe4cd2272a5473ce081a380b2cea
titus <titustesche@outlook.de>
2025-02-01 23:08:44 +0100

    Added temporary script for future implementation of image generation

---
+++ src/scripts/image-gen.py
@@ -0,0 +1,38 @@
+import torch
+from diffusers import BitsAndBytesConfig, StableDiffusion3Pipeline
+
+model_path = "/models/<modelfile/folder>"
+
+# 4-Bit-Quantisierung (FP4)
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="fp4",
+    bnb_4bit_compute_dtype=torch.float16
+)
+
+pipeline = StableDiffusion3Pipeline.from_pretrained(
+    model_path,
+    torch_dtype=torch.bfloat16,
+    quantization_config=nf4_config,
+    # load_in_4bit=True,
+    # bnb_4bit_quant_type="fp4",
+    # bnb_4bit_compute_dtype=torch.float16
+)
+
+pipeline.enable_attention_slicing()
+pipeline.enable_sequential_cpu_offload()
+
+prompt = (
+    "Your super cool original nft"
+)
+
+image = pipeline(
+    prompt=prompt,
+    num_inference_steps=28,
+    guidance_scale=4.5,
+    width=512,
+    height=512,
+    max_sequence_length=256,
+).images[0]
+
+image.save("image.png")
