2c131e191951ff3c3dce60c924605886e758beb6
titus <titustesche@outlook.de>
2025-08-25 18:01:58 +0200

    Fixed a bug where sometimes an email or username would be too short to generate a UUID string for that user, added session token to response when registering

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,21 +1,29 @@
 import {Router} from "express";
 import {z} from "zod";
-import { v4 as uuidv4 } from "uuid";
+import {v4 as uuidv4} from "uuid";
 import {User} from "../database/entities/User";
 import {processRequestBody} from "zod-express-middleware";
-const bcrypt = require("bcrypt");
+import bcrypt = require("bcrypt");
+import * as crypto from "node:crypto";
+import * as jwt from 'jsonwebtoken';
+import {JWT_SECRET} from "./auth/authMiddleware";
 
-const saltRounds = 10;
-
+// How should registration data be structured?
 const registerSchema = z.object({
     name: z.string(),
-    email: z.string(),
+    email: z.string().email(),
     password: z.string(),
 
 })
 const userRoute = Router();
 
-userRoute.post("/user", processRequestBody(registerSchema), async (req, res) => {
+
+
+userRoute.post('/user/login', async (req, res) => {
+    const user = await User.findOne({})
+});
+
+userRoute.post("/user/register", processRequestBody(registerSchema), async (req, res) => {
     const user = await User.findOne({
         where: {
             email: req.body.email,
@@ -24,22 +32,36 @@ userRoute.post("/user", processRequestBody(registerSchema), async (req, res) => 
 
     if (!user) {
         const newUser = new User();
+
+        // Overcomplicated creation of a user ID based on the user's email address
         const options = {
-            random: Buffer.from(req.body.email),
+            random:
+                crypto.createHash('sha256')
+                .update(req.body.email)
+                .digest()
+                .subarray(0, 16)
         };
 
         newUser.id = uuidv4(options);
         newUser.name = req.body.name;
         newUser.email = req.body.email;
-        await bcrypt.hash(req.body.password, saltRounds, async (err: any, hash: string) => {
-            if (err) {
-                return res.status(500);
-            }
-            newUser.passwordHash = hash;
+        try {
+            // Hash the password
+            newUser.passwordHash = await bcrypt.hash(req.body.password, 10);
+            // Save the user
             await newUser.save();
-        });
+            // Create a new access token for the client
+            const token = jwt.sign({userId: newUser.id, email: newUser.email}, JWT_SECRET, { expiresIn: '24h'});
+            // Respond back with the token
+            return res.status(201).json({token: token});
+        }
 
-        return res.status(201).json({uuid: newUser.id});
+        // In case something goes wrong
+        catch (e) {
+            // Log that error and report back to the client
+            console.error(e);
+            return res.status(500).json({error: e});
+        }
     }
 
     return res.status(400).json({error: "User already exists"});
